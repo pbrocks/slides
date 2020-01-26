ca0fd8d1b498995522f72162cf833177653ceeca
Paul Barthmaier <paul@barthmaier.rocks>
2020-01-26 07:50:45 -0500

    spacing

--- src/code-editor.js
+++ src/code-editor.js
@@ -1,34 +1,38 @@
+// var editor = wp.codeEditor.initialize($('#my_textarea'), my_var.cm_settings);
+// var errors = codeEditor.find('.CodeMirror-lint-marker-error');
+// console.log(errors.length);
+
 const {
-  element: { createElement: e, useRef, useEffect, memo },
-  codeEditor: { initialize, defaultSettings }
+	element: { createElement: node, useRef, useEffect, memo },
+	codeEditor: { initialize, defaultSettings }
 } = window.wp;
 
-export default memo(({ onChange, mode, ...props }) => {
-  const ref = useRef();
+export default memo( ( { onChange, mode, ...props } ) => {
+	const ref = useRef();
 
-  useEffect(() => {
-    const editor = initialize(ref.current, {
-      ...defaultSettings,
-      codemirror: {
-        ...defaultSettings.codemirror,
-        tabSize: 2,
-        mode,
-        lineNumbers: false
-      }
-    });
+	useEffect(() => {
+		const editor = initialize( ref.current, {
+			...defaultSettings,
+			codemirror: {
+				...defaultSettings.codemirror,
+				tabSize: 4,
+				mode,
+				lineNumbers: false
+			}
+		});
 
-    editor.codemirror.on('change', () => {
-      onChange(editor.codemirror.getValue());
-    });
+		editor.codemirror.on('change', () => {
+			onChange( editor.codemirror.getValue() );
+		});
 
-    return () => {
-      editor.codemirror.toTextArea();
-    };
-  });
+		return () => {
+			editor.codemirror.toTextArea();
+		};
+	});
 
-  return e('textarea', {
-    ref,
-    ...props
-  });
+	return node( 'textarea', {
+		ref,
+		...props
+	});
   // Never rerender.
 }, () => true);
